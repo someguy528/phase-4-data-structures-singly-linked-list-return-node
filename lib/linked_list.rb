83236c83b24da3ee5d27834ae25b43410cde19c8
Brandon Castro <castroyou999@gmail.com>
2023-02-24 21:22:01 -0500

    commit tree cc70878ebf23c8841cf9d80436fb1192237396d2

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,39 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    # stack = []
+    # last_node = head
+    # while last_node != nil
+    #   stack.push(last_node)
+    #   last_node = last_node.next_node
+    # end
+    # x = n
+    # while x > 1 && stack.size > 0
+    #   stack.pop
+    #   x -= 1
+    # end
+    # if stack.length > 0 && n > 0
+    #   stack[stack.length-1].value
+    # else
+    #   nil
+    # end
+    return nil unless head
+    x = 0
+    last_node = head
+    while last_node
+      last_node = last_node.next_node
+      x += 1
+    end
+    return nil if n > x || n < 1
+
+    last_node = head
+    i = 0
+    while i < x - n 
+      last_node = last_node.next_node
+      i += 1
+    end
+    last_node.value
+
   end
 
 end
